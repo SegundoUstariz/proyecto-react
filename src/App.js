5f7286d620d045a68efecf3f39327f42475ba55d
Valen Ulloa <valenulloa@MacBook-Pro-de-Valen.local>
2022-09-01 18:41:29 -0300

    prueba

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React !!!! hola jsos hola probando
+          Learn React !!!! hola jsos hola probando!!
         </a>
       </header>
     </div>
