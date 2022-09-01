92782fe70be1245935b9f7a63fbaf29fb95eec2a
sustariz@udesa.edu.ar <sustariz@udesa.edu.ar>
2022-09-01 18:26:42 -0300

    hola

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React !!!! hola jsos
+          Learn React
         </a>
       </header>
     </div>
