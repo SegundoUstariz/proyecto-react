b88f24d927fc7be50c5221da2179228ad1386ddf
valenulloa <69592234+valenulloa@users.noreply.github.com>
2022-09-08 16:55:37 -0300

    card logic

--- src/components/CardComponents/CardComponents.js
+++ src/components/CardComponents/CardComponents.js
@@ -1,21 +1,39 @@
+import { toHaveStyle } from '@testing-library/jest-dom/dist/matchers';
 import React, { Component } from 'react';
 
 class CardComponents extends Component {
-    constructor(props) {
+    constructor(props){
         super(props)
         this.state = {
+            verMas: false        }
+    }
 
-        }
+    verMas(){
+        this.setState ({
+            verMas: !this.state.verMas}) 
     }
 
     render() {
         console.log(this.props);
         return (
             <article className='character-card'>
-                <img src={this.props.datosPela.poster_path} alt="" />
+                <img src={"https://image.tmdb.org/t/p/w500/"+this.props.datosPela.poster_path} alt="" />
                 <h2>{this.props.datosPela.original_title}</h2> 
-                <p>{this.props.datosPela.overview}</p> 
-                <p>Ver más</p>
+
+                   {
+                    this.state.verMas ? <p>{this.props.datosPela.overview}</p> : <p></p>
+                   }
+
+                <button onClick={() => this.verMas()}>
+                    {this.state.verMas ? "Ver Menos" : "Ver Mas" }
+                </button>
+                <button>
+                    Ir a detalle
+                </button>
+                <button>
+                    Anadir a Favoritos
+                </button>
+
             </article>
 
         )
--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Card(){
-    return (
-        <article className="data-detail">
-            <div className="card-content">
-                <h4>título de ficha</h4>
-                <p>datos de ficha</p>
-            </div>
-            <i className="fas fa-clipboard-list fa-2x text-gray-300"></i>
-        </article>
-    )
+class Card extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            verMas: false,
+            text: "Ver Mas"
+        }
+    }
+
+    
 }
 
 export default Card;
