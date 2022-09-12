00abaa68036b690187ee9deba83d7567469712d6
francescaporro <50020152+francescaporro@users.noreply.github.com>
2022-09-12 12:01:34 -0300

    Revert "Merge branch 'master' of https://github.com/SegundoUstariz/proyecto-react"
    This reverts commit 74db6d3b0681ce97c2f2ca656550cd264831a121, reversing
    changes made to 2178f1924374161bd5b911f0085004d8bb4720d6.

--- src/index.js
+++ src/index.js
@@ -3,15 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Link, Route, Switch} from 'react-router-dom'
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+root.render(<App />)
+
 
